9abe584cbd6f44cb17811b53ec9b3ba950c94d79
Daniel Moon <dii@ornl.gov>
2024-05-31 12:56:34 -0400

    Commit

---
+++ armstrong.py
@@ -0,0 +1,9 @@
+import math
+
+def find_square_root(num):
+    return math.sqrt(num)
+
+number = float(input("Enter a number: "))
+
+square_root = find_square_root(number)
+print(f"The square root of {number} is {square_root}.")
